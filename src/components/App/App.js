e01af087d91a0c6866ccab2875ebabfaeb666780
gs195 <timedilation01@gmail.com>
2019-08-12 03:46:53 +0800

    Add chart

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,7 +4,16 @@ import { getPeriods } from "../../services/timePeriodService";
 import { getStashawayReturns } from "../../services/stashawayReturnsService";
 import { getMarketReturns } from "../../services/marketReturnsService";
 import PeriodBar from "../periodBar/periodBar";
-import { VictoryChart, VictoryLine, VictoryTheme, VictoryAxis } from "victory";
+import CurrencyBar from "../currencyBar/currencyBar";
+
+import {
+  VictoryChart,
+  VictoryLine,
+  // VictoryTheme,
+  VictoryAxis,
+  VictoryPortal,
+  VictoryLabel
+} from "victory";
 
 class App extends React.Component {
   constructor(props) {
@@ -12,14 +21,15 @@ class App extends React.Component {
     this.state = {
       periods: [],
       selectedPeriod: "1 month",
+      selectedCurrency: "SGD",
       stashawayReturns: [],
       marketReturns: []
     };
   }
 
-  updateReturns = () => {
-    const newStashawayReturns = getStashawayReturns(this.state.selectedPeriod);
-    const newMarketReturns = getMarketReturns(this.state.selectedPeriod);
+  updateReturns = (period, currency) => {
+    const newStashawayReturns = getStashawayReturns(period, currency);
+    const newMarketReturns = getMarketReturns(period, currency);
     this.setState({
       stashawayReturns: newStashawayReturns,
       marketReturns: newMarketReturns
@@ -29,13 +39,22 @@ class App extends React.Component {
   componentDidMount() {
     const newPeriods = getPeriods();
     this.setState({ periods: newPeriods }, () => {
-      this.updateReturns();
+      this.updateReturns(
+        this.state.selectedPeriod,
+        this.state.selectedCurrency
+      );
     });
   }
 
   handleSelectPeriod = periodName => {
     this.setState({ selectedPeriod: periodName }, () => {
-      this.updateReturns();
+      this.updateReturns(periodName, this.state.selectedCurrency);
+    });
+  };
+
+  handleSelectCurrency = currency => {
+    this.setState({ selectedCurrency: currency }, () => {
+      this.updateReturns(this.state.selectedPeriod, currency);
     });
   };
 
@@ -48,9 +67,42 @@ class App extends React.Component {
           periods={this.state.periods}
           handleSelectPeriod={this.handleSelectPeriod}
         />
+        <CurrencyBar
+          selectedCurrency={this.state.selectedCurrency}
+          handleSelectCurrency={this.handleSelectCurrency}
+        />
         <div className="chart">
-          <VictoryChart theme={VictoryTheme.material}>
+          <VictoryChart
+            // theme={VictoryTheme.material}
+            width={700}
+            height={350}
+            // style={{ parent: { maxWidth: "90%" } }}
+          >
+            <VictoryAxis
+              // tickValues={(1, 2, 3, 4, 5, 6, 7)}
+              label="Time"
+              style={{
+                axisLabel: { marginTop: 35 }
+              }}
+            />
+            <VictoryAxis
+              dependentAxis
+              // theme={VictoryTheme.material}
+              tickLabelComponent={
+                <VictoryPortal>
+                  <VictoryLabel />
+                </VictoryPortal>
+              }
+              label="Returns in dollars"
+              style={{
+                axisLabel: { marginTop: 35 },
+                grid: { stroke: 0.5 },
+                ticks: { stroke: "grey", size: 5 },
+                tickLabels: { fontSize: 15, padding: 5 }
+              }}
+            />
             <VictoryLine
+              interpolation="natural"
               style={{
                 data: { stroke: "#c43a31" },
                 parent: { border: "1px solid #ccc" }
@@ -58,8 +110,9 @@ class App extends React.Component {
               data={this.state.stashawayReturns}
             />
             <VictoryLine
+              interpolation="natural"
               style={{
-                data: { stroke: "#c43a41" },
+                data: { stroke: "rgb(129, 129, 255)" },
                 parent: { border: "1px dashed #ccc" }
               }}
               data={this.state.marketReturns}
