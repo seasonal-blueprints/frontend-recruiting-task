0e26cbc3321b43721a414854cbf41dbb16a8a52b
gs195 <timedilation01@gmail.com>
2019-08-12 03:45:09 +0800

    Update periodBar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,39 +1,73 @@
-import React from 'react';
-import './app.css';
-import { getPeriods } from "../../services/timePeriodService"
-import { getStashawayReturns } from "../../services/stashawayReturnsService"
-import { getMarketReturns } from "../../services/marketReturnsService"
-import PeriodBar from "../periodBar/periodBar"
+import React from "react";
+import "./app.css";
+import { getPeriods } from "../../services/timePeriodService";
+import { getStashawayReturns } from "../../services/stashawayReturnsService";
+import { getMarketReturns } from "../../services/marketReturnsService";
+import PeriodBar from "../periodBar/periodBar";
+import { VictoryChart, VictoryLine, VictoryTheme, VictoryAxis } from "victory";
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       periods: [],
-      selectedPeriod: "monthly",
+      selectedPeriod: "1 month",
       stashawayReturns: [],
       marketReturns: []
     };
   }
 
-  componentDidMount(){
-    this.setState({periods: getPeriods()});
-    this.setState({stashawayReturns: getStashawayReturns(this.state.selectedPeriod)})
-    this.setState({marketReturns: getMarketReturns(this.state.selectedPeriod)})
+  updateReturns = () => {
+    const newStashawayReturns = getStashawayReturns(this.state.selectedPeriod);
+    const newMarketReturns = getMarketReturns(this.state.selectedPeriod);
+    this.setState({
+      stashawayReturns: newStashawayReturns,
+      marketReturns: newMarketReturns
+    });
+  };
+
+  componentDidMount() {
+    const newPeriods = getPeriods();
+    this.setState({ periods: newPeriods }, () => {
+      this.updateReturns();
+    });
   }
 
-  handlePeriodClick = (periodName) => {
-    this.setState({selectedPeriod: periodName})
-  }
-
+  handleSelectPeriod = periodName => {
+    this.setState({ selectedPeriod: periodName }, () => {
+      this.updateReturns();
+    });
+  };
 
   render() {
-  return (
-    <div className="App">
-      <h1>recruitment assignment</h1>
-      <PeriodBar selectedPeriod={this.state.selectedPeriod} periods={this.state.periods} handlePeriodClick={this.handlePeriodClick} />
-    </div>
-  );
+    return (
+      <div className="App">
+        <h1>recruitment assignment</h1>
+        <PeriodBar
+          selectedPeriod={this.state.selectedPeriod}
+          periods={this.state.periods}
+          handleSelectPeriod={this.handleSelectPeriod}
+        />
+        <div className="chart">
+          <VictoryChart theme={VictoryTheme.material}>
+            <VictoryLine
+              style={{
+                data: { stroke: "#c43a31" },
+                parent: { border: "1px solid #ccc" }
+              }}
+              data={this.state.stashawayReturns}
+            />
+            <VictoryLine
+              style={{
+                data: { stroke: "#c43a41" },
+                parent: { border: "1px dashed #ccc" }
+              }}
+              data={this.state.marketReturns}
+            />
+          </VictoryChart>
+        </div>
+      </div>
+    );
   }
 }
 
--- src/components/periodBar/periodBar.js
+++ src/components/periodBar/periodBar.js
@@ -1,21 +1,28 @@
-import React from "react"
-import './periodBar.css';
+import React from "react";
+import "./periodBar.css";
 
-const PeriodBar = ({selectedPeriod, periods, handlePeriodClick}) => {
-    const getClass = (periodName, period) => {
-        if (period.name === periodName) return "selected"
-    return "unselected"
-
-    }
-    return (
-        <div className="periodBar__container">
-            {periods.map(period => {
-                return <button id={period.name} key={period._id} type="button" className={getClass(selectedPeriod, period)}  onClick={() => handlePeriodClick(period.name)}>
-                    {period.name}
-                </button>
-            })}
-        </div>
-    );
+const PeriodBar = ({ selectedPeriod, periods, handleSelectPeriod }) => {
+  const getClass = (periodName, period) => {
+    if (period.name === periodName) return "selected";
+    return "unselected";
+  };
+  return (
+    <div className="periodBar__container">
+      {periods.map(period => {
+        return (
+          <button
+            id={period.name}
+            key={period._id}
+            type="button"
+            className={getClass(selectedPeriod, period)}
+            onClick={() => handleSelectPeriod(period.name)}
+          >
+            {period.name}
+          </button>
+        );
+      })}
+    </div>
+  );
 };
 
 export default PeriodBar;
