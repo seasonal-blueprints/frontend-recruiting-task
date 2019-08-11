370d16421eeb562a57cc6e0e61aea4722d9aa9a1
gs195 <timedilation01@gmail.com>
2019-08-12 01:52:07 +0800

    Update mock data

--- src/services/marketReturnsService.js
+++ src/services/marketReturnsService.js
@@ -1,18 +1,31 @@
 let portfolioReturns = [
   {
-    id: "monthly",
-    data:[
-    {Jul01:100000},
-    {Jul05:135000},
-    {Jul10:120000},
-    {Jul15:170000},
-    {Jul20: 190000},
-    {Jul25: 215000},
-    {Jul30: 240000}
-  ]
+    id: "1 month",
+    data: [
+      { x: "Jul01", y: 100000 },
+      { x: "Jul05", y: 135000 },
+      { x: "Jul10", y: 120000 },
+      { x: "Jul15", y: 170000 },
+      { x: "Jul20", y: 190000 },
+      { x: "Jul25", y: 215000 },
+      { x: "Jul30", y: 240000 }
+    ]
+  },
+  {
+    id: "6 months",
+    data: [
+      { x: "Jun", y: 100000 },
+      { x: "Jul", y: 150000 },
+      { x: "Aug", y: 220000 },
+      { x: "Sept", y: 210000 },
+      { x: "Oct", y: 240000 },
+      { x: "Nov", y: 250000 },
+      { x: "Dec", y: 260000 }
+    ]
   }
 ];
 
 export function getMarketReturns(id) {
- return portfolioReturns.find(investmentReturns => investmentReturns.id === id).data;
+  return portfolioReturns.find(investmentReturns => investmentReturns.id === id)
+    .data;
 }
--- src/services/stashawayReturnsService.js
+++ src/services/stashawayReturnsService.js
@@ -1,19 +1,36 @@
 let portfolioReturns = [
   {
-    id: "monthly",
+    id: "1 month",
     data: [
-    {Jul01:100000},
-    {Jul05:140000},
-    {Jul10:125000},
-    {Jul15:185000},
-    {Jul20: 220000},
-    {Jul25: 265000},
-    {Jul30: 295000}
+      { x: "Jul01", y: 100000 },
+      { x: "Jul05", y: 140000 },
+      { x: "Jul10", y: 125000 },
+      { x: "Jul15", y: 185000 },
+      { x: "Jul20", y: 220000 },
+      { x: "Jul25", y: 265000 },
+      { x: "Jul30", y: 295000 }
+    ]
+  },
+  {
+    id: "6 months",
+    data: [
+      { x: "Jun", y: 100000 },
+      { x: "Jul", y: 160000 },
+      { x: "Aug", y: 225000 },
+      { x: "Sept", y: 220000 },
+      { x: "Oct", y: 255000 },
+      { x: "Nov", y: 270000 },
+      { x: "Dec", y: 275000 }
     ]
   }
 ];
 
 export function getStashawayReturns(id) {
-  return portfolioReturns.find(investmentReturns => investmentReturns.id === id).data;
+  if (!id) {
+    return portfolioReturns.find(
+      investmentReturns => investmentReturns.id === "1 month"
+    ).data;
+  }
+  return portfolioReturns.find(investmentReturns => investmentReturns.id === id)
+    .data;
 }
-
